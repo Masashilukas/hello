00b7a4d7a2a37f70bd8e163afae2e0a5b1cbfc30
Lukas DeGraw <ldegraw2@huskers.unl.edu>
2022-02-02 02:37:47 +0000

    added major to output

--- hello.c
+++ hello.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 int main(int argc, char **argv){
-    
-    printf("Lukas Degraw\n");
+
+    printf("Lukas Degraw ;)\n");
+    printf("\tElectrical Engineering\n");
     return(0);
-    
+
 }
